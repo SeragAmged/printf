327412003c18597b05ec379546d203d5831df7e1
omar546 <omer.mkawy.om@gmail.com>
2023-04-26 03:21:56 +0200

    deferent approach

--- _printf.c
+++ _printf.c
@@ -1,47 +1,90 @@
+#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
+
 /**
-* _printf - main function to print in console
-* @format: array to print and check type
-* Return: count of character printed
-**/
+ * find_function - Find the function corresponding to the format specifier.
+ * @format: The format specifier.
+ *
+ * Return: A pointer to the corresponding function, or NULL if not found.
+ */
+int (*find_function(const char *format))(va_list)
+{
+    code_f formats[] = {
+        {"c", print_char},
+        {"s", print_string},
+        {"i", print_int},
+        {"d", print_dec},
+        {"r", print_rev},
+        {"b", print_bin},
+        {"u", print_unsig},
+        {"o", print_octal},
+        {"x", print_x},
+        {"X", print_X},
+        {"R", print_rot13},
+        {NULL, NULL}
+    };
+    int i;
+
+    for (i = 0; formats[i].sc != NULL; i++)
+    {
+        if (*(formats[i].sc) == *format)
+            return (formats[i].f);
+    }
+
+    return (NULL);
+}
+
+/**
+ * _printf - Print a formatted string to the standard output.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed (excluding the null byte).
+ */
 int _printf(const char *format, ...)
 {
-	int count = -1;
+    va_list args;
+    int (*f)(va_list);
+    int count = 0;
+    size_t i;
 
-	if (format != NULL)
-	{
-		int i;
-		va_list ar_list;
-		int (*o)(va_list);
+    if (format == NULL)
+        return (-1);
 
-		va_start(ar_list, format);
+    va_start(args, format);
 
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            _putchar(format[i]);
+            count++;
+        }
+        else
+        {
+            f = find_function(&format[i + 1]);
+            if (f != NULL)
+            {
+                count += f(args);
+                i++;
+            }
+            else if (format[i + 1] == '%')
+            {
+                _putchar('%');
+                count++;
+                i++;
+            }
+            else if (format[i + 1] != '\0')
+            {
+                _putchar('%');
+                count++;
+            }
+            else
+                return (-1);
+        }
+    }
 
-		count = 0;
+    va_end(args);
 
-		for (i = 0; format[i] != '\0'; i++)
-		{
-			if (format[i] == '%')
-			{
-				if (format[i + 1] == '%')
-				{
-					count += _putchar(format[i]);
-					i++;
-				}
-				else if (format[i + 1] != '\0')
-				{
-					o = get_func(format[i + 1]);
-					count += (o ? o(ar_list) : _putchar(format[i]) + _putchar(format[i + 1]));
-					i++;
-				}
-			}
-			else
-				count += _putchar(format[i]);
-		}
-		va_end(ar_list);
-	}
-
-	return (count);
+    return (count);
 }
--- main.h
+++ main.h
@@ -1,14 +1,32 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
+
 #include <stdarg.h>
 
-typedef struct op
-{
-	char *c;
-	int (*f)(va_list);
-} op_t;
-
-int _printf(const char *format, ...);
 int _putchar(char c);
-#endif
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_int(va_list args);
+int print_dec(va_list args);
+int print_rev(va_list args);
+int print_bin(va_list args);
+int print_unsig(va_list args);
+int print_octal(va_list args);
+int print_x(va_list args);
+int print_X(va_list args);
+int print_rot13(va_list args);
+
+/**
+ * struct code_format - Struct for format specifier and corresponding function.
+ *
+ * @sc: The format specifier.
+ * @f: The function corresponding to the format specifier.
+ */
+typedef struct code_format
+{
+    char *sc;
+    int (*f)(va_list);
+} code_f;
+
+#endif /* MAIN_H */
