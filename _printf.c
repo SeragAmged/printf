8a677106c3d6be3552c8aa3547fe45f9b34e41af
SeragAmged <seragamged2002@gmail.com>
2023-04-26 02:30:09 +0200

    fix

--- _printf.c
+++ _printf.c
@@ -1,54 +1,54 @@
 #include "main.h"
-/**
- * _printf - Produces output according to a format
- * @format: A character string containing zero or more directives
- *
- * Return: The number of characters printed (excluding the null byte used to
- *         end output to strings), or -1 on failure
- */
 
-int _printf(const charformat, ...)
+/**
+* _printf - Prints output to stdout according to a format.
+* @format: Format string.
+*
+* Return: The number of characters printed (excluding the null byte used
+* to end output to strings).
+*/
+int _printf(const char *format, ...)
 {
-int count = 0;
 va_list args;
-char c, s;
+int count = 0;
 
 va_start(args, format);
 
-while (format)
+while (*format != '\0')
 {
-if (format == '%')
+if (*format == '%')
 {
 format++;
-
-switch (format)
+switch (*format)
 {
 case 'c':
-c = (char) va_arg(args, int);
+{
+int c = va_arg(args, int);
 putchar(c);
 count++;
 break;
-
+}
 case 's':
-s = va_arg(args, char );
-while (s)
 {
-putchar(s);
+char *s = va_arg(args, char*);
+while (*s != '\0')
+{
+putchar(*s);
 s++;
 count++;
 }
 break;
-
+}
 case '%':
+{
 putchar('%');
 count++;
 break;
-
+}
 default:
-putchar('%');
-putchar(format);
-count += 2;
+{
 break;
+}
 }
 }
 else
@@ -56,11 +56,9 @@ else
 putchar(*format);
 count++;
 }
-
 format++;
 }
 
 va_end(args);
-
-return count;
+return (count);
 }
