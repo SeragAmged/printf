39eff724c64673555e85f1e05b44b12d8bdc247e
omar546 <omer.mkawy.om@gmail.com>
2023-04-26 02:52:31 +0200

    code fix

--- _printf.c
+++ _printf.c
@@ -1,65 +1,90 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * _printf - Prints output to stdout according to a format.
- * @format: Format string.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: The number of characters printed (excluding the null byte used
- * to end output to strings).
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
+/**
+ * _printf - prints output according to a format
+ * @format: character string containing format specifiers
+ *
+ * Return: number of characters printed (excluding null byte used to end output to strings)
+ */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
+	const char *str;
+	va_list arg;
+	int printed_chars = 0;
 
-va_start(args, format);
+	va_start(arg, format);
 
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-switch (*format)
-{
-case 'c':
-{
-int c = va_arg(args, int);
-putchar(c);
-count++;
-break;
-}
-case 's':
-{
-char *s = va_arg(args, char*);
-while (*s != '\0')
-{
-putchar(*s);
-s++;
-count++;
-}
-break;
-}
-case '%':
-{
-putchar('%');
-count++;
-break;
-}
-default:
-{
-break;
-}
-}
-}
-else
-{
-putchar(*format);
-count++;
-}
-format++;
+	for (str = format; str && *str; str++)
+	{
+		if (*str != '%')
+		{
+			_putchar(*str);
+			printed_chars++;
+		}
+		else if (*str == '%' && *(str + 1) == '%')
+		{
+			_putchar('%');
+			printed_chars++;
+			str++;
+		}
+		else if (*str == '%' && *(str + 1) != '\0')
+		{
+			switch (*(str + 1))
+			{
+			case 'c':
+				_putchar(va_arg(arg, int));
+				printed_chars++;
+				break;
+			case 's':
+				printed_chars += print_string(va_arg(arg, char *));
+				break;
+			default:
+				_putchar(*str);
+				printed_chars++;
+				break;
+			}
+			str++;
+		}
+		else
+		{
+			_putchar(*str);
+			printed_chars++;
+		}
+	}
+	va_end(arg);
+
+	return (printed_chars);
 }
 
-va_end(args);
-return count;
+/**
+ * print_string - prints a string
+ * @str: the string to print
+ *
+ * Return: the number of characters printed
+ */
+int print_string(char *str)
+{
+	int i;
+
+	if (str == NULL)
+		str = "(null)";
+
+	for (i = 0; str[i]; i++)
+		_putchar(str[i]);
+
+	return (i);
 }
